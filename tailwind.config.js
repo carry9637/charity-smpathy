50005d6e13e951fbd64951ff9884e9520db07317
carry9637 <kartipaul366@gmail.com>
2026-08-02 11:40:47 +0000

    Configure Tailwind with tw prefix

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./webheady.com/Charity-sympathy/**/*.html"
+  ],
+  prefix: "tw-",
+  corePlugins: {
+    preflight: false
+  },
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
